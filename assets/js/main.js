08c14bef20ea08631cb113e7110ba6fb94414036
Aaron Gilmore <aaron@aarongilmore.com>
2019-05-26 13:37:50 +0100

    Fixing MailChimp form on mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 $( "c h2" ).addClass( "mw7" );
 
-// Get the container element
-var btnContainer = document.getElementById("nav");
+// // Get the container element
+// var btnContainer = document.getElementById("nav");
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("nav-link");
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("nav-link");
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//       var current = document.getElementsByClassName("active");
   
-      // If there's no active class
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
+//       // If there's no active class
+//       if (current.length > 0) {
+//         current[0].className = current[0].className.replace("active", "");
+//       }
   
-      // Add the active class to the current/clicked button
-      this.className += " active";
-    });
-  } 
+//       // Add the active class to the current/clicked button
+//       this.className += " active";
+//     });
+//   } 
 
 // // Hide Header on on scroll down
 // var didScroll;
